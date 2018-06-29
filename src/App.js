df0ca5fa8bdb02da02566dfb0df9b527f20ef0f9
hellomkreyes <mkgreyes@gmail.com>
2018-06-29 09:33:19 -0400

    added api key, but receiving 403 bad access error. must debug!

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>Hi!</h1>
         <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
